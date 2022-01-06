3b9a29afbab5d35dde3575c37b5d9fe6e7948ba1
Ankit kumar sahoo <65510958+d-riddle@users.noreply.github.com>
2022-01-07 03:55:23 +0530

    trying heroku deploy again

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL: "https://the-daily-blog-app.herokuapp.com/"
+    baseURL: " https://the-daily-blog-app.herokuapp.com/api/"
 });
